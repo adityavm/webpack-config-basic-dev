277e3d0eecdc1a52f46f784a96eeab26423e2d09
Aditya Mukherjee <aditya@adityamukherjee.com>
2018-02-20 11:51:57 +0530

    turn off CleanWebpackPlugin messages

--- conf/webpack.production.config.js
+++ conf/webpack.production.config.js
@@ -21,7 +21,7 @@ module.exports = dirname => {
       new webpack.DefinePlugin({
         "process.env.NODE_ENV": JSON.stringify("production"),
       }),
-      new CleanWebpackPlugin(appDir, { root: dirname }),
+      new CleanWebpackPlugin(appDir, { root: dirname, verbose: false }),
       new HtmlWebpackPlugin(),
       new UglifyJsPlugin({
         parallel: true,
